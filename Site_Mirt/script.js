faa338237d517323d3a9451f176dbef09f258860
glfac-iscteiul <145680845+glfac@users.noreply.github.com>
2025-03-15 22:59:40 +0000

    Add files via upload

---
+++ Site_Mirt/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let button = document.querySelector(".btn");
+    button.addEventListener("click", function () {
+        alert("Entre em contato conosco para comprar os melhores mirtilos!");
+    });
+});
