1ba9879759dc799511dfa2e9f7dca89883669da3
glfac-iscteiul <145680845+glfac@users.noreply.github.com>
2025-03-19 23:39:11 +0000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let button = document.querySelector(".btn");
-    button.addEventListener("click", function () {
-        alert("Entre em contato conosco para comprar os melhores mirtilos!");
-    });
-});
